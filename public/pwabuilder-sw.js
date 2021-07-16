f4c20b54157f6bc880503a6a35aa6125deffb4f5
KhoiNguyen <thorwaitson@gmail.com>
2021-07-16 11:07:16 -0500

    update service worker

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,7 +1,3 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
-
 const CACHE = "pwabuilder-offline";
 const HTML_CACHE = "html";
 const JS_CACHE = "javascript";
